882a08be668d8191be2db086493b33870524d87d
David Hurka <doxydoxy@mailbox.org>
2022-05-08 15:55:57 +0200

    Support for SAMD21 family: Copy doc-samd21.h from sam/d
    This file defines the two top level doxygen groups for SAM libraries:
    peripheral_apis and SAMD21_defines.
    
    Files like sam/d21/port.h will later define subgroups like PORT_defines,
    which are the same across families and specific to hardware modules.
    These groups will be used by sam/common/port.h and similar.
    
    The group SAMD21 is not (yet?) included,
    like in ../d/doc-samd.h, and unlike in STM32.
    I do not know why groups like STM32F1xx are used.

---
+++ include/libopencm3/sam/d21/doc-samd21.h
@@ -0,0 +1,8 @@
+/** @defgroup peripheral_apis Peripheral APIs
+ * APIs for device peripherals
+ */
+
+/** @defgroup SAMD21_defines SAMD21 Defines
+ * Defined Constants and Types for the SAMD21 series.
+ * @copyright SPDX: LGPL-3.0-or-later
+ */
